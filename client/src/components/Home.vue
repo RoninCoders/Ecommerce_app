1ce80a85a35158741a912b76ca746751363d8914
thiago <thiago.lopes.dev@gmail.com>
2021-02-06 11:43:34 -0300

    chore: initialize project

---
+++ client/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>App</div>
+  <div>{{ msg }}</div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Home',
+  props: {
+    msg: String
+  }
+})
+
+</script>
